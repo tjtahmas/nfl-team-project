7735749bcbd6c15ee0e725a0b4b6cd89e5b29c70
Montii <llh9@yahoo.com>
2021-12-18 21:21:05 -0500

    No Errors

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,7 +7,7 @@ function Header(props) {
   return (
     <header className="flex-row space-between px-1">
       <div>
-      <h1>NFL Fan Banter<img class='nflLogo' src={nflLogo}></img></h1>
+      <h1>NFL Fan Banter<img className='nflLogo' src={nflLogo}></img></h1>
       <p>Your team isn't that bad... You just need the right stats</p>
       </div>
       <img src='https://www.teahub.io/photos/full/93-931010_american-football-field-hd.jpg' alt="wooden background"></img>
