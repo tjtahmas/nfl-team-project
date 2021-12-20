5211ff9c139b3661c5eafcfe72db5ad8c3f775c3
Jalon Tuttle <jalontuttle@gmail.com>
2021-12-20 12:44:45 -0500

    updating styling throughout pages

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import coverImage from '../../assets/cover/Brain-Tap-3.jpg';
 import nflLogo from '../../assets/logos/foulfb.png'
 
 function Header(props) {
@@ -8,9 +7,8 @@ function Header(props) {
     <header className="flex-row space-between px-1">
       <div>
       <h1>NFL Fan Banter<img className='nflLogo' src={nflLogo}></img></h1>
-      <p>Your team isn't that bad... You just need the right stats</p>
       </div>
-      <img src='https://www.teahub.io/photos/full/93-931010_american-football-field-hd.jpg' alt="wooden background"></img>
+      <img src='https://wallpapercave.com/wp/ZSM48St.jpg' alt="background"></img>
       {props.children}
     </header>
   );
