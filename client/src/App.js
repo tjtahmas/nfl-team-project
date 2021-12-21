8c8a1f9ea37cadee095431cd9355ca2f2c4aa7ea
Jalon Tuttle <jalontuttle@gmail.com>
2021-12-21 10:26:04 -0500

    attempting to deploy with stats 2

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 
 const client = new ApolloClient({
-  uri: 'http://localhost:3001/graphql',
+  uri: '/graphql',
   cache: new InMemoryCache(),
 });
 function App() {
