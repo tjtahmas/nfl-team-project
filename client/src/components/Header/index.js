3654b55bfad9adaa73be3e10e9fa1a1f336fb77d
Jalon Tuttle <jalontuttle@gmail.com>
2021-12-21 10:29:40 -0500

    changed app name in header

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -6,7 +6,7 @@ function Header(props) {
   return (
     <header className="flex-row space-between px-1">
       <div>
-      <h1>NFL Fan Banter<img className='nflLogo' src={nflLogo}></img></h1>
+      <h1>NFL Wing Man<img className='nflLogo' src={nflLogo}></img></h1>
       </div>
       <img src='https://wallpapercave.com/wp/ZSM48St.jpg' alt="background"></img>
       {props.children}
